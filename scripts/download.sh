eb4b6f5fd3d74e8b7c4b665baf47705f8423358b
john <1165137798@qq.com>
2025-02-09 12:58:03 +0800

    commit

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,2 +1,2 @@
 # 3. Download Valid-Numpy.
-wget -P /root/VIRTUAL_imagenet256_labeled.npz  https://openaipublic.blob.core.windows.net/diffusion/jul-2021/ref_batches/imagenet/256/VIRTUAL_imagenet256_labeled.npz
+wget -P /root  https://openaipublic.blob.core.windows.net/diffusion/jul-2021/ref_batches/imagenet/256/VIRTUAL_imagenet256_labeled.npz
