4f0e30b7b2d175e72924fa91ebe61f86003cf6e0
john <1165137798@qq.com>
2025-02-09 23:15:16 +0800

    commit

--- state/state_pjit.py
+++ state/state_pjit.py
@@ -60,7 +60,6 @@ def get_torch_model_from_rar_size(rar_model_size='rar_xxl'):
     # local_dir= '../'
     local_dir= '/root/'
     # local_dir= 'torch_model_weight/'
-wei
     class ConfigTokenizer:
         channel_mult = [1, 1, 2, 2, 4]
         num_resolutions = 5
