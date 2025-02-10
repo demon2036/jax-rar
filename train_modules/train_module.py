bf1cf867a8703eeb74f62af826f6abbe94b4a994
john <1165137798@qq.com>
2025-02-10 13:25:38 +0800

    commit

--- train_modules/train_module.py
+++ train_modules/train_module.py
@@ -64,6 +64,9 @@ class TrainModule(nn.Module):
         condition=self.model.preprocess_condition(labels,self.make_rng('dropout'),cond_drop_prob=0.1)
         logits=self.model.train_dpo(tokens, condition)
 
+
+        print(labels.shape,logits.shape)
+
         loss=optax.softmax_cross_entropy_with_integer_labels(logits[:,:-1],labels)
         return {'loss':loss.mean()}
 
